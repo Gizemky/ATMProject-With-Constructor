d3982d97107b35d22caba8a8a3ae524060f26ee4
Gizem KAYA <77019634+Gizemky@users.noreply.github.com>
2021-05-07 14:23:22 +0300

    Update main.java

--- ATMProject_Constructor/src/main.java
+++ ATMProject_Constructor/src/main.java
@@ -3,11 +3,11 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Atm atm = new Atm (); //atm objesi oluþturma.
+		Atm atm = new Atm (); 
 		
-		Accountt account = new Accountt ("Gizem KAYA","123456Gk",4000); //hesap objesi oluþturma.
+		Accountt account = new Accountt ("Gizem KAYA","123456Gk",4000); 
 		
-		atm.work(account);        //yukarýda oluþturduðumuz hesap objesini work metoduna  göndermek gerekiyor.Aksi durumda hata alýrýz.
+		atm.work(account);        
 		
 		System.out.println("Exiting the program !");
 
